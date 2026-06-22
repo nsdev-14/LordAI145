a10524e4f4adaa3a221eaa7a954afa86cc03bcbf
NSK14 <nagaprasoon1418compu@gmail.com>
2026-06-22 14:40:55 +0000

    Fix

--- src/components/lord/AppShell.tsx
+++ src/components/lord/AppShell.tsx
@@ -104,7 +104,7 @@ export function AppShell({ children }: { children: ReactNode }) {
 
     return () => {
       mounted = false;
-      data.subscription.unsubscribe();
+      authListener?.subscription.unsubscribe();
     };
   }, [navigate]);
 
